17710e465221442f46c0b41a8f025b3d0cbec82f
Can Arda <canarda.dev@gmail.com>
2023-05-07 04:26:10 +0200

    Add files via upload

---
+++ Js HA/script.js
@@ -0,0 +1,76 @@
+let Deutschland = "53 million"
+let Hannover = "4 million"
+
+console.log (Deutschland);
+console.log (Hannover);
+
+let isIsland = true;
+console.log (typeof isIsland);
+
+Isisland = "Deutschland"
+console.log (typeof Isisland);
+
+let language;
+console.log (language);
+console.log (typeof language);
+
+const massMarks = 78;
+const heightMarks = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95; 
+
+const BMIMark = massMarks / heightMarks ** 2; 
+const BMIJohn = massJohn / heightJohn ** 2; 
+const markHigherBMI = BMIMark > BMIJohn; 
+
+console.log(BMIMark , BMIJohn, markHigherBMI);
+
+
+
+const massMark = 95;
+const heightMark = 1.88; 
+const massJohn = 85;
+const heightJohn = 1.76;
+const BMIMark = massMark / heightMark ** 2; 
+const BMIJohn = massJohn / heightJohn ** 2;
+const markHigherBMI = BMIMark > BMIJohn; 
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+
+
+
+// in Deutschland Wohnen Leute :
+const DeutschlandEinwohnerZahl = 84000000;
+
+//Falls halb Personen verlassen Deutschland: 
+const ifDeutschlandhalf = DeutschlandEinwohnerZahl / 2; 
+
+//in Finland Wohnen Leute:
+const FinlandEinwohnerZahl = 6000000;
+
+//Deutschland oder Finland WohnerZahlen: 
+const DeutschlandEinwohnerZahloderFinlandEinwohnerZahl = DeutschlandEinwohnerZahl > FinlandEinwohnerZahl;
+
+//durschnittlichLand in die Welt 
+const durchschnittlichLand = 33000000; 
+
+//durschnittlichLand oder Deutschland
+const deutschlandDurschnittlichLandoder = durchschnittlichLand > DeutschlandEinwohnerZahl; 
+
+console.log(ifDeutschlandhalf, DeutschlandEinwohnerZahloderFinlandEinwohnerZahl, deutschlandDurschnittlichLandoder);
+
+
+
+const massCem = 102;
+const heightCem = 1.98;
+const massCan = 78; 
+const heightCan = 1.78;
+
+const CemBMI = massCem / heightCem ** 2;
+const CanBMI = massCan / heightCan ** 2; 
+
+const CemBMIhigher = CemBMI > CanBMI; 
+
+console.log(CemBMI, CanBMI, CemBMIhigher);
+
